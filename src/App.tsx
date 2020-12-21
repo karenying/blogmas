f21164bbc030285c22d952163d4b2ee95d3d3edc
Karen Ying <karenying7@gmail.com>
2020-12-20 20:15:20 -0500

    Link post in header

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,22 @@ import './App.scss';
 
 const App = () => (
   <div className='App'>
-    <div className='title'>
-      bl
-      <span className='dark-pink-text'>o</span>
-      gm
-      <span className='blue-text'>a</span>s{' '}
-      <span className='title-year'>
-        2<span className='light-pink-text'>0</span>2
-        <span className='yellow-text'>0</span>
-      </span>
-    </div>
+    <a
+      href='https://blog.karenying.com/posts/blogmas-2020'
+      target='_blank'
+      rel='noreferrer'
+    >
+      <div className='title'>
+        bl
+        <span className='dark-pink-text'>o</span>
+        gm
+        <span className='blue-text'>a</span>s{' '}
+        <span className='title-year'>
+          2<span className='light-pink-text'>0</span>2
+          <span className='yellow-text'>0</span>
+        </span>
+      </div>
+    </a>
 
     <div className='subtitle'>
       Miniposts every day of December until Christmas 🎊
